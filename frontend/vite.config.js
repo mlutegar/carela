6be4dda00400c91279bf7c375a8b231a3c6da9bf
mlutegar <mlutegar@gmail.com>
2026-05-13 22:42:56 -0300

    auto: alterações 22:42:56

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,10 @@ import { defineConfig, loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   const apiUrl = env.VITE_API_URL || "http://localhost:8000";
+  const isProd = mode === "production";
 
   return {
+    base: isProd ? "/carela/" : "/",
     plugins: [react()],
     server: {
       proxy: {
